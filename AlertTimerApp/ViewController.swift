3c19221d6496d2af2d6535db5c4494f17caa16eb
yyeonjju <llilyha123@gmail.com>
2023-02-08 16:46:46 +0900

    Initial Commit

---
+++ AlertTimerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertTimerApp
+//
+//  Created by 하연주 on 2023/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
